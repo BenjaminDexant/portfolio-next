76e83007654365ab7c61a1c2556c6dfb6b73bbe2
BenjaminDexant <61704427+BenjaminDexant@users.noreply.github.com>
2021-06-01 11:51:01 +0200

    npm install ts & tailwind and init project

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import "../styles/globals.css";
+
+function MyApp({ Component, pageProps }) {
+	return (
+		<div>
+			<div>Sidebar</div>
+			<div>
+				<Component {...pageProps} />
+			</div>
+		</div>
+	);
+}
+
+export default MyApp;
